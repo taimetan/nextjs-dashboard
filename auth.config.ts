40da7c0817c78b493df44e1c45ebbd81f05a00c7
taimetan <67098238+taimetan@users.noreply.github.com>
2024-12-10 15:32:32 +0700

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,6 @@
-import { NextAuthConfig } from 'next-auth';
+import type { NextAuthConfig } from 'next-auth';
 
-export const authConfig: NextAuthConfig = {
+export const authConfig = {
     pages: {
         signIn: '/login',
     },
@@ -11,6 +11,17 @@ export const authConfig: NextAuthConfig = {
             }
             return baseUrl + '/dashboard';
         },
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+            if (isOnDashboard) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
     },
-    providers: [],
-};
+    providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
